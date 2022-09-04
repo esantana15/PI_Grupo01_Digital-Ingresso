3afb6996302133e5435a52d389fafd8d481bd919
lucaspertile <lucaspts@gmail.com>
2022-09-04 13:50:28 -0300

    corrigindo a rota src

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const { v4 } = require('uuid');
 
-let db_clientes = require('../scr/database/db_clientes.json');
+let db_clientes = require('../src/database/db_clientes.json');
 
 const writeToDB = () => {
     const json = JSON.stringify(db_clientes);
-    fs.writeFileSync('scr/database/db_clientes.json', json);
+    fs.writeFileSync('src/database/db_clientes.json', json);
 }
 
 const Cliente = {
